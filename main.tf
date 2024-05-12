c73579d3205f83fdad24881bbf6873f14cd5c355
MD ISLAM <nrislam@gmail.com>
2024-05-11 22:50:15 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -78,7 +78,7 @@ module "ec2" {
 
   name = local.name
 
-  ami                         = data.aws_ami.amazon_linux.id
+  ami                         = data.aws_ami.amazon-linux-2.id
   instance_type               = "c5.large"
   availability_zone           = local.availability_zone
   subnet_id                   = element(module.vpc1.private_subnets, 0)
